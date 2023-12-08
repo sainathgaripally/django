dbeeb1c6739cee520d56812df7808b9953efb894
gitdevops <gitdevops@gmail.com>
2023-12-08 20:34:13 +0530

    nf

--- somepath/main.py
+++ somepath/main.py
@@ -15,19 +15,21 @@ for key, value in new_name.items():
 tfdatapath = '/home/azureuser/python_json_tfvars/django/modules/terr.tfvars'
 with open(tfdatapath,'r') as tfvarfile:
     tfdata = tfvarfile.readlines()
+    new_list =  []
 
 print(tfdata)
 
 print("########################################")
 
 for key, value in new_name.items():
-    # print(key+"printed")
     for text in tfdata:
         values = text.split('=')
         variable_name = values[0].strip()
         variable_value = values[1].strip()
-        # print(variable_name)
-        # print(variable_value)
         if(key == variable_name):
-            print(variable_name + "its wow")
+            print(variable_name)
+
+with open(newfile, w) as w:
+    for text in tfdata:
+        w.write(text)
 
