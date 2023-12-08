2602d23634b1feb91f828c4d8e76be6c33a476b8
gitdevops <gitdevops@gmail.com>
2023-12-08 20:56:27 +0530

    nf

--- somepath/main.py
+++ somepath/main.py
@@ -28,12 +28,14 @@ print("########################################")
 #     for text in new_list:
 #         w.write(text)
 
+print(new_list)
+
 for key, value in new_name.items():
     for text in tfdata:
         values = text.split('=')
         variable_name = values[0].strip()
         variable_value = values[1].strip()
-        if(key in new_list):
+        if(key in variable_name):
             print(variable_name)
             print(variable_value)
             print("Yes")
