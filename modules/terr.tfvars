13a92d316e4042672afea920d708e30765927d17
gitdevops <gitdevops@gmail.com>
2023-12-08 19:17:03 +0530

    nf

---
+++ modules/terr.tfvars
@@ -0,0 +1,3 @@
+subscription_id = "xxxx"
+resources       = "demo1-rg"
+location        = "East US"
---
+++ somepath/main.py
@@ -0,0 +1,13 @@
+ort json 
+import os
+ 
+manifest_path = './values.json'
+with open(manifest_path,'r') as manifest_file:
+    data = json.load(manifest_file)
+
+print(data)
+    
+new_name = data["regions"]["region"]["resource_group"]
+
+for key, value in new_name.items():
+    print(f"Key: {key}, Value: {value}")
